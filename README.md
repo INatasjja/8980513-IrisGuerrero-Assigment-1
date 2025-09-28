3b8ec43fa5fde02b27577adf087f87ee429318a1
Iris Guerrero <irisnatashia@gmail.com>
2025-09-28 12:45:45 -0400

    White and black box test

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+# Hash text Sorurce:
+- https://it-tools.tech/hash-text
--- test.py
+++ test.py
@@ -34,6 +34,46 @@ class TestHashingResults(unittest.TestCase):
             result.stdout
         )
 
+    def test_md5_output(self):
+        inputPassword="Github"
+        inputHashtype="md5"
+        outputExpectedResult="e1adbcbb92c622d0b3e619f9d0730abf"
+        result = fileCaller(inputPassword,inputHashtype)
+        #Verify that the call to the main file is correct. 
+        self.assertEqual(result.returncode, 0)
+        #Verify result
+        self.assertIn(
+            outputExpectedResult,
+            result.stdout
+        )
+    
+    def test_sha1_output(self):
+        inputPassword="Github"
+        inputHashtype="sha1"
+        outputExpectedResult="c53ced31f785a1888b348de05057011fedd3be48"
+        result = fileCaller(inputPassword,inputHashtype)
+        #Verify that the call to the main file is correct. 
+        self.assertEqual(result.returncode, 0)
+        #Verify result
+        self.assertIn(
+            outputExpectedResult,
+            result.stdout
+        )
+
+    def test_sha512_output(self):
+        inputPassword="Github"
+        inputHashtype="sha512"
+        outputExpectedResult="567efaa953d9c7f53865ab6efca82ddd1031d772503352c5ac992f0ca7eef88ddf523ba7e1d049b0d3559941679697be2b874bfb68e5fc0dc8eb37aa204fcca9"
+        result = fileCaller(inputPassword,inputHashtype)
+        #Verify that the call to the main file is correct. 
+        self.assertEqual(result.returncode, 0)
+        #Verify result
+        self.assertIn(
+            outputExpectedResult,
+            result.stdout
+        )
+
+        ##White Box Test
     def test_nohashtype(self):
         inputPassword="Github"
         inputHashtype=""
@@ -47,9 +87,18 @@ class TestHashingResults(unittest.TestCase):
             result.stdout
         )
 
-
-
-
+    def test_noInputPassword(self):
+        inputPassword=""
+        inputHashtype=""
+        outputExpectedResult=""
+        result = fileCaller(inputPassword,inputHashtype)
+        #Verify that the call to the main file is correct. 
+        self.assertEqual(result.returncode, 0)
+        #Verify result
+        self.assertIn(
+            outputExpectedResult,
+            result.stdout
+        )
 
 if __name__ == "__main__":
     unittest.main()
